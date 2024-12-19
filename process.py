81e6217a90728f5d19f70b517ad93ab31fe03b41
SunsetOS <weihou.huang@gmail.com>
2024-12-19 21:22:58 +0800

    Add mark to the data

--- process.py
+++ process.py
@@ -22,4 +22,25 @@ def ModifyCSV():
                 writer.writerows(data)
             print("File", i, "is save to csv")
 
-ModifyCSV()
+def Mark():
+    for i in range(1,11):
+        with open(f'./PostProcess/noheader/notdrop/{i:0>2d}.csv', 'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            for j in data:
+                j.append('0')
+            with open(f'./PostProcess/noheader/notdrop/{i:0>2d}.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+            print("File", i, "is tagged")
+    for i in range(1,11):
+        with open(f'./PostProcess/noheader/drop/{i:0>2d}.csv', 'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            for j in data:
+                j.append('1')
+            with open(f'./PostProcess/noheader/drop/{i:0>2d}.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+            print("File", i, "is tagged")
+Mark()
