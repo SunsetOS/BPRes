acada58691052c00e9d79ac917b6019d5bd84f71
SunsetOS <weihou.huang@gmail.com>
2024-12-19 23:14:47 +0800

    Merge csv

--- process.py
+++ process.py
@@ -61,4 +61,35 @@ def Mark():
                 writer.writerows(data)
             print("File", i, "is marked")
 
-Mark()
+def Merge():
+    for i in range(1,6):
+        # Merge Train
+        with open(f"./PostProcess/noheader/drop/{i:0>2d}.csv", "r") as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            with open(f"./PostProcess/mergedTrain.csv", "a", newline = "") as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+        with open(f"./PostProcess/noheader/notdrop/{i:0>2d}.csv", "r") as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            with open(f"./PostProcess/mergedTrain.csv", "a", newline = "") as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+    print("Merged")
+    # Merge Test
+    for i in range(6,11):
+        with open(f"./PostProcess/noheader/drop/{i:0>2d}.csv", "r") as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            with open(f"./PostProcess/mergedTest.csv", "a", newline = "") as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+        with open(f"./PostProcess/noheader/notdrop/{i:0>2d}.csv", "r") as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            with open(f"./PostProcess/mergedTest.csv", "a", newline = "") as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+    print("Merged")
+Merge()
