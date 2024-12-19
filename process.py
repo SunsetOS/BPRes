3ac35707ab6e2bbc7ed6474477a67c6202f89fed
SunsetOS <weihou.huang@gmail.com>
2024-12-19 22:41:36 +0800

    Remove blank

--- process.py
+++ process.py
@@ -2,13 +2,13 @@ import pandas as pd
 import csv
 def ConverFile():
     for i in range(1,11):
-        data = pd.read_excel(f'./notdrop/{i:0>2d}.xlsx')
-        data.to_csv(f'./output/notdrop/{i:0>2d}.csv', index=False)
+        data = pd.read_excel(f'./PreProcess/notdrop/{i:0>2d}.xlsx')
+        data.to_csv(f'./PreProcess/output/notdrop/{i:0>2d}.csv', index=False)
         print("File", i, "is converted to csv")
 
 def ModifyCSV():
     for i in range(1,11):
-        with open(f'./output/notdrop/{i:0>2d}.csv', 'r') as f:
+        with open(f'./PostProcess/output/notdrop/{i:0>2d}.csv', 'r') as f:
             reader = csv.reader(f)
             data = list(reader)
             data.pop(0)
@@ -17,10 +17,10 @@ def ModifyCSV():
                 j.pop(0)
                 j.pop(0)
                 j.pop(0)
-            with open(f'./output/noheader/notdrop/{i:0>2d}.csv', 'a', newline='') as f:
+            with open(f'./PostProcess/noheader/notdrop/{i:0>2d}.csv', 'w', newline = '') as f:
                 writer = csv.writer(f)
                 writer.writerows(data)
-            print("File", i, "is save to csv")
+            print("File", i, "save to csv")
 
 def Mark():
     for i in range(1,11):
@@ -29,7 +29,7 @@ def Mark():
             data = list(reader)
             for j in data:
                 j.append('0')
-            with open(f'./PostProcess/noheader/notdrop/{i:0>2d}.csv', 'w') as f:
+            with open(f'./PostProcess/noheader/notdrop/{i:0>2d}.csv', 'w', newline = "") as f:
                 writer = csv.writer(f)
                 writer.writerows(data)
             print("File", i, "is tagged")
@@ -39,8 +39,9 @@ def Mark():
             data = list(reader)
             for j in data:
                 j.append('1')
-            with open(f'./PostProcess/noheader/drop/{i:0>2d}.csv', 'w') as f:
+            with open(f'./PostProcess/noheader/drop/{i:0>2d}.csv', 'w', newline = "") as f:
                 writer = csv.writer(f)
                 writer.writerows(data)
             print("File", i, "is tagged")
-Mark()
+
+ModifyCSV()
